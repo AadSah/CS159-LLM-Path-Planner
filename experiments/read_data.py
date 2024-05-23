b24f0db1354a378b2992665c665daac94e946d9c
Ziqi Ma <ziqima@Ziqis-MBP-2.lan>
2024-05-22 22:58:56 -0700

    tested llm part

--- experiments/read_data.py
+++ experiments/read_data.py
@@ -2,15 +2,15 @@ import json
 
 # this fetches the full json object of one game configuration
 # mode is train/dev/test/unseen
-def get_json(num_goals, mode, idx):
-    path = f"data/json_with_symbols/obs-2/{num_goals}_goals/{num_goals}_goals_{mode}_set_samples_with_symbols.json"
+def get_world_config(num_goals, mode, idx):
+    path = f"../data/json_with_symbols/obs-2/{num_goals}_goals/{num_goals}_goals_{mode}_set_samples_with_symbols.json"
     with open(path) as file:
         worlds = json.load(file)
     return worlds[idx]
 
 def get_image_path(num_goals, mode, idx):
-    path = f"data/colored_box_pictures/obs-2/{num_goals}_goals/{num_goals}_goals_{mode}_set_samples/{num_goals}_goals_{mode}_set_samples_{idx}.png"
-    return worlds[i]
+    path = f"../data/colored_box_pictures/obs-2/{num_goals}_goals/{num_goals}_goals_{mode}_set_samples/{num_goals}_goals_{mode}_set_samples_{idx+1}.png"
+    return path
 
 def get_symbolic_description(world):
     return world["symbolic_representation"]
